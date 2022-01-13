1354ab63981f60a1f71558a3addacf489d879d1b
sreekeshiyer <sreekesh.subramanian@gmail.com>
2022-01-13 20:41:03 +0530

    🎨 Result Box Updated Horizontal Scrollbar

--- components/ResultBox.js
+++ components/ResultBox.js
@@ -1,6 +1,6 @@
 export default function ResultBox({ result }) {
     return (
-        <div className="bg-white p-3 w-[500px] h-[400px] m-[2rem] overflow-auto">
+        <div className="bg-white p-3 w-[500px] h-[400px] m-[2rem] overflow-auto result-box">
             <code className="text-black">{result}</code>
         </div>
     );
